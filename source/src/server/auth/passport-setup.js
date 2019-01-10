42ca70ed4a93b163176a574c0e6a9417780409bf
Yousof Mersal <yousof777@gmail.com>
2019-01-10 22:02:42 +0100

    refactor server folder and fix database errors

--- source/src/server/auth/passport-setup.js
+++ source/src/server/auth/passport-setup.js
@@ -1,23 +1,35 @@
-const database = require('../database')
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+})
+
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 //serialize or "encrypt" a user into a cookie to send to the client
 passport.serializeUser((user, done) => {
-  console.log(user)
-  done(null, user.id)
+  done(null, user.google_id)
 })
 
 //deserialize or "decrypt" the cookie into readable information
 passport.deserializeUser((id, done) => {
-  database
-    .getUserFromId(id)
-    .then(results => {
-      done(null, results[0])
-    })
-    .catch(err => {
-      throw err
-    })
+  pool.query(
+    'select * FROM users where google_id = ?',
+    [id],
+    (err, results, fields) => {
+      if (err) {
+        throw new Error('Something went wrong while retriving a users id!\n' + err)
+      } else {
+        done(null, results[0])
+      }
+    }
+  )
 })
 
 passport.use(
@@ -31,47 +43,54 @@ passport.use(
       callbackURL: process.env.CALLBACK
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(profile._json)
-      //passport callbaack, that uses our mysql database to first check if we already have a user with the unique google_id
-
-      const googleID = profile._json.id
-      const fullName = profile._json.displayName
-      const imgURL = profile._json.image.url
-
-      database
-        .getUserFromGoogleId(googleID)
-        .then(results => {
-          if (
+      //passport callbaack, that uses our mysql database to first check if we already have a user with the unique github_ID
+      pool.query(
+        'select * FROM users WHERE google_id = ?',
+        [profile._json.id],
+        async (err, results, fields) => {
+          if (err) {
+            throw new Error('Something went wrong in fething a user!' + err)
+          } else if (
             results === undefined ||
             results.length === 0 ||
             results[0].google_id === undefined
           ) {
-            database
-              .createNewUserFromGoogleId(googleID, fullName, imgURL)
-              .then(response => {
-                database.getUserFromId(response.insertId).then(results => {
-                  done(null, results[0])
-                  return
-                })
-                // const user = {
-                //   id : response.insertId,
-                //   google_id : googleID,
-                //   full_name : fullName,
-                //   img_url : imgURL
-                // }
-                // done(null, user);
-                // return
-              })
+            //If no user is found with the provided id, create one.
+            pool.query(
+              'INSERT INTO users (google_id, name, google_login, type, avatar) VALUES( ?, ?, ?, ?, ?)',
+              [
+                profile._json.id,
+                profile._json.name.givenName,
+                profile._json.displayName,
+                profile._json.kind,
+                profile._json.image.url
+              ],
+              (err, results, fields) => {
+                if (err) {
+                  throw new Error('Whoops! could not add Google User to DB!' + err)
+                } else {
+                  const user = {
+                    id: results.insertId,
+                    avatar: profile._json.image.url,
+                    name: profile._json.name.givenName,
+                    type: profile._json.kind,
+                    google_login: profile._json.displayName,
+                    google_id: profile._json.id
+                  }
+                  //then send to passport.js for serialization the user we just created and tell it that we're done
+                  //checking if the user exists
+                  done(null, user)
+                  return user
+                }
+              }
+            )
           } else {
-            console.log('user founded' + results)
             //if user already exists and there is no need for a new user send the existing user to passport.js for serialization
             done(null, results[0])
-            return
+            return results[0]
           }
-        })
-        .catch(err => {
-          throw err
-        })
+        }
+      )
     }
   )
 )
