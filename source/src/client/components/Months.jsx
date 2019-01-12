92dd2a872a356446ed5979ac72cc29716f895510
Yousof Mersal <yousof777@gmail.com>
2019-01-12 19:40:59 +0100

    add moment.js

--- source/src/client/components/Months.jsx
+++ source/src/client/components/Months.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 export default class Months extends Component {
+  getMonth(prop) {
+    var months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    var now = new Date()
+    var thisMonth = months[now.getMonth() + prop] // getMonth method returns the month of the date (0-January :: 11-December)
+    return thisMonth
+  }
   render() {
     return (
       <div className='monthComp'>
-        <h5>this month</h5>
+        <div className='monthName'>
+          <h5 className='thisMonth'>{this.getMonth(0)}</h5>
+          <h5 className='nextMonth'>{this.getMonth(1)}</h5>
+        </div>
       </div>
     )
   }
