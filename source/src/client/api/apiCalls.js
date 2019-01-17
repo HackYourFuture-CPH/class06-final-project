c44c989a883cb9058b81cf98d9a5f0c82190625b
Yousof Mersal <yousof777@gmail.com>
2019-01-17 22:05:23 +0100

    Refactor api calls in client and add fetch for module options on server and client

--- source/src/client/api/apiCalls.js
+++ source/src/client/api/apiCalls.js
@@ -1,25 +1,29 @@
 import Axios from 'axios'
 
+const errfunc = err => {
+  if (err.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(err.response.data)
+    console.log(err.response.status)
+    console.log(err.response.headers)
+  } else if (err.request) {
+    // The request was made but no response was received
+    throw new Error(
+      'Whoops something went wring while POSTing to server!: ' + err.request
+    )
+  } else {
+    // Something happened in setting up the request that triggered an err
+    throw new Error(
+      'Whoops something went wring while POSTing to server!: ' + err.response
+    )
+  }
+  console.log(err.config)
+}
+
 async function getProfileInfo() {
   const response = await Axios.post('/api/profile', { withCredentials: true }).catch(
-    err => {
-      if (err.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(err.response.data)
-        console.log(err.response.status)
-        console.log(err.response.headers)
-      } else if (err.request) {
-        // The request was made but no response was received
-        throw new Error('Whoops something went wring while POSTing!: ' + err.request)
-      } else {
-        // Something happened in setting up the request that triggered an err
-        throw new Error(
-          'Whoops something went wring while POSTing!: ' + err.response
-        )
-      }
-      console.log(err.config)
-    }
+    err => errfunc(err)
   )
   return response
 }
@@ -54,69 +58,32 @@ async function isLoggedIn() {
 async function createNewClass(params) {
   console.log(params)
   const response = await Axios.post('/api/createclass', { data: params }).catch(
-    err => {
-      if (err.response) {
-        console.log(err.response.data)
-        console.log(err.response.status)
-        console.log(err.response.headers)
-      } else if (err.request) {
-        throw new Error('Whoops something went wring while POSTing!: ' + err.request)
-      } else {
-        throw new Error(
-          'Whoops something went wring while POSTing!: ' + err.response
-        )
-      }
-      console.log(err.config)
-    }
+    err => errfunc(err)
   )
   return await response.data
 }
 
 async function getClasses() {
-  const res = await Axios.post('/api/getclasses').catch(err => {
-    if (err.response) {
-      console.log(err.response.data)
-      console.log(err.response.status)
-      console.log(err.response.headers)
-    } else if (err.request) {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.request)
-    } else {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.response)
-    }
-    console.log(err.config)
-  })
+  const res = await Axios.post('/api/getclasses').catch(err => errfunc(err))
   return await res.data
 }
 
 async function updateClass(data) {
-  const res = await Axios.post('/api/updateclass', { data }).catch(err => {
-    if (err.response) {
-      console.log(err.response.data)
-      console.log(err.response.status)
-      console.log(err.response.headers)
-    } else if (err.request) {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.request)
-    } else {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.response)
-    }
-    console.log(err.config)
-  })
+  const res = await Axios.post('/api/updateclass', { data }).catch(err =>
+    errfunc(err)
+  )
   return await res.data
 }
 
 async function deleteClass(data) {
-  const res = await Axios.post('/api/deleteclass', { data }).catch(err => {
-    if (err.response) {
-      console.log(err.response.data)
-      console.log(err.response.status)
-      console.log(err.response.headers)
-    } else if (err.request) {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.request)
-    } else {
-      throw new Error('Whoops something went wring while POSTing!: ' + err.response)
-    }
-    console.log(err.config)
-  })
+  const res = await Axios.post('/api/deleteclass', { data }).catch(err =>
+    errfunc(err)
+  )
+  return await res.data
+}
+
+async function getModuleOptions() {
+  const res = await Axios.post('/api/getmoduleoptions').catch(err => errfunc(err))
   return await res.data
 }
 
@@ -126,5 +93,6 @@ export {
   createNewClass,
   getClasses,
   updateClass,
-  deleteClass
+  deleteClass,
+  getModuleOptions
 }
