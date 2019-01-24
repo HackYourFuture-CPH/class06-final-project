b242a3fb43e9e643db56c44031925481bb30f273
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-01-24 06:53:49 +0100

    feat: adjust how the button should be in order on the navbar

--- source/src/client/components/NavBar.jsx
+++ source/src/client/components/NavBar.jsx
@@ -1,40 +1,56 @@
 import React from "react";
 import { Pane, Button } from "evergreen-ui";
 import { Link } from "react-router-dom";
+import hyfLogo from "../assets/logo.svg";
 
 export const NavBar = props => {
   return (
-    <Pane display="flex" padding={16} background="tealTint">
-      {props.isAuthenticated && (
-        <Button>
-          <Link to="/profile">Profile</Link>
-        </Button>
-      )}
-      <Link to="/adminview">
-        <Button marginRight={12}>admin view</Button>
-      </Link>
-      <Link to="/">
-        <Button marginRight={12} onClick={() => console.log("clicked")}>
-          Landing Page
-        </Button>
-      </Link>
+    <Pane
+      display="flex"
+      justifyContent="space-between"
+      paddingLeft={16}
+      paddingRight={16}
+      paddingTop={8}
+      paddingBottom={8}
+      background="tealTint"
+    >
+      <Pane width={300} height={50}>
+        <img
+          src={hyfLogo}
+          alt="logo"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Pane>
+      <Pane display="flex" alignItems="center">
+        <Link to="/">
+          <Button marginRight={12}>Home</Button>
+        </Link>
+        <Link to="/adminview">
+          <Button marginRight={12}>Admin</Button>
+        </Link>
+        {props.isAuthenticated && (
+          <Button>
+            <Link to="/profile">Profile</Link>
+          </Button>
+        )}
 
-      <Button
-        appearance="primary"
-        intent="success"
-        marginRight={12}
-        iconBefore="log-in"
-      >
-        <a
-          href={
-            props.isAuthenticated
-              ? "http://localhost:9001/auth/google/logout"
-              : "http://localhost:9001/auth/google"
-          }
+        <Button
+          appearance="primary"
+          intent="success"
+          marginRight={12}
+          iconBefore={props.isAuthenticated ? "log-out" : "log-in"}
         >
-          {props.isAuthenticated ? "Logout" : "Login"}
-        </a>
-      </Button>
+          <a
+            href={
+              props.isAuthenticated
+                ? "http://localhost:9001/auth/google/logout"
+                : "http://localhost:9001/auth/google"
+            }
+          >
+            {props.isAuthenticated ? "Logout" : "Login"}
+          </a>
+        </Button>
+      </Pane>
     </Pane>
   );
 };
